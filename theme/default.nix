2c10449663567df84595e82606eabd179f982f77
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2023-09-13 21:09:38 +0100

    comment

--- theme/default.nix
+++ theme/default.nix
@@ -8,6 +8,7 @@
   options.theme =
     config.lib.theme.mkThemeCommonOptions { inherit (config) theme; };
 
+  # Seems to be the most practical solution for internalising libs for modules
   config.lib.theme = (import ./lib {
     inherit (inputs) base16;
     inherit lib pkgs;
